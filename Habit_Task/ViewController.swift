0a8c8b590011f97f933e9ac043631b403349d375
MoaazBelalCGI <m.elkazzaz96@gmail.com>
2025-01-14 15:13:11 +0200

    UI Implementation

--- Habit_Task/ViewController.swift
+++ Habit_Task/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var habitTextField: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var completeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        statusLabel.isHidden = true
+        completeButton.layer.cornerRadius = completeButton.frame.height / 2
     }
-
+    @IBAction func completedButtonPressed(_ sender: UIButton) {
+        guard let habitName = habitTextField.text, !habitName.isEmpty else {
+            statusLabel.isHidden = false
+            statusLabel.text = "Please enter a habit name!"
+            statusLabel.textColor = .systemRed
+            return
+        }
+        
+        statusLabel.isHidden = false
+        statusLabel.text = "You completed '\(habitName)' for today!"
+        statusLabel.textColor = .systemGreen
+    }
 
 }
 
